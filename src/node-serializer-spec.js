6fc6d09d070e6b14d68d6df22791497c43341728
Juan Carlos Medina <juanca.med@gmail.com>
2017-11-13 19:47:05 -0800

    Add some grouping to test output

--- src/node-serializer-spec.js
+++ src/node-serializer-spec.js
@@ -1,5 +1,7 @@
 import nodeSerializer from './node-serializer.js';
 
+console.group('node-serializer-spec');
+
 const rootNode1 = window.document.createElement('a');
 rootNode1.href = 'http://www.google.com';
 rootNode1.textContent = 'Visit Google!';
@@ -9,3 +11,5 @@ console.log('serialized node:', serializedRootNode1);
 console.log('TAG NAME?', serializedRootNode1.tagName == 'A');
 console.log('HREF?', serializedRootNode1.href == 'http://www.google.com');
 console.log('TEXT CONTENT?', serializedRootNode1.textContent == 'Visit Google!');
+
+console.groupEnd('node-serializer-spec');
--- src/virtual-vs-dom-check-spec.js
+++ src/virtual-vs-dom-check-spec.js
@@ -1,6 +1,8 @@
 import nodeSerializer from './node-serializer.js';
 import virtualVsDomCheck from './virtual-vs-dom-check.js';
 
+console.group('virtual-vs-dom-check');
+
 const rootNode1 = window.document.createElement('a');
 rootNode1.href = 'http://www.google.com';
 rootNode1.textContent = 'Visit Google!';
@@ -12,3 +14,5 @@ const rootNode2 = window.document.createElement('a');
 rootNode2.href = 'http://www.google.com';
 rootNode2.textContent = 'Visit Google!!!';
 console.log('rootNode1 check:', virtualVsDomCheck(virtualRootNode1, rootNode2) === false);
+
+console.groupEnd('virtual-vs-dom-check');
