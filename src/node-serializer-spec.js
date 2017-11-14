ba772623833871c60975beb5b62162c6760d1dcd
Juan Carlos Medina <juanca.med@gmail.com>
2017-11-13 19:23:26 -0800

    Initial specification for serializing shallow node

--- src/node-serializer-spec.js
+++ src/node-serializer-spec.js
@@ -1,3 +1,11 @@
 import nodeSerializer from './node-serializer.js';
 
-console.log('it works!', nodeSerializer());
+const rootNode1 = window.document.createElement('a');
+rootNode1.href = 'http://www.google.com';
+rootNode1.textContent = 'Visit Google!';
+
+const serializedRootNode1 = nodeSerializer(rootNode1);
+console.log('serialized node:', serializedRootNode1);
+console.log('TAG NAME?', serializedRootNode1.tagName == 'a');
+console.log('HREF?', serializedRootNode1.href == 'http://www.google.com');
+console.log('TEXT CONTENT?', serializedRootNode1.textContent == 'Visit Google!');
